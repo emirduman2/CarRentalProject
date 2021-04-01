fdadfd01d1c7faf4b2fe92e97c57afaff213d864
Emir Can Ünlütaş <s4.icin1@hotmail.com>
2021-04-01 09:15:24 +0300

    JWT Support Added, somethings refactored.

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
